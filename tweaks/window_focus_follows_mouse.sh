9206a50bc1e6bea101fe24a9e24aa681b97908d9
Hayden Donald <haydendonald@gmail.com>
2022-11-06 16:49:55 +1100

    Update window_focus_follows_mouse.sh

--- tweaks/window_focus_follows_mouse.sh
+++ tweaks/window_focus_follows_mouse.sh
@@ -1,6 +1,6 @@
 # Make window focus follow the mouse
-#https://github.com/koekeishiya/yabai/wiki
-brew install koekeishiya/formulae/yabai
+brew tap koekeishiya/formulae
+brew install yabai
+mkdir -p ~/.config/yabai/
+printf 'yabai -m config focus_follows_mouse autofocus' >> ~/.config/yabai/yabairc
 brew services start yabai
-yabai -m config focus_follows_mouse autofocus
-echo "If it hasn't started enable in security and reboot ;)"
