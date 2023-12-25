fd0b121f87ded8748525cf27b8e5055fdba91e60
Hayden Donald <haydendonald@gmail.com>
2023-12-25 12:51:58 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 # Run all the scripts!
 
 echo "Lets go!"
+
+chmod +x ./installs/install_xcode.sh
+chmod +x ./installs/install_homebrew.sh
+chmod +x ./installs/install_apps.sh
+chmod +x ./installs/mac_settings.sh
+
 sh ./installs/install_xcode.sh
 sh ./installs/install_homebrew.sh
 sh ./installs/install_apps.sh
 sh ./tweaks/mac_settings.sh
-sh ./tweaks/window_focus_follows_mouse.sh
